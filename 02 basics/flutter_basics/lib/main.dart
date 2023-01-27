01b0de9472e21fde8407d4d2d60b50cd113270d9
Christian Adler <chris.adler@gmx.de>
2023-01-27 11:09:04 +0100

    add first layout Widget

--- 02 basics/flutter_basics/lib/main.dart
+++ 02 basics/flutter_basics/lib/main.dart
@@ -10,13 +10,22 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = ['What\'s your favorite color?', 'Favorite animal?'];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My first App'),
           backgroundColor: Color.fromRGBO(102, 0, 255, 1.0),
         ),
-        body: Text('body text'),
+        body: Column(
+          children: <Widget>[
+            Text('the question'),
+            ElevatedButton(onPressed: null, child: Text('Answer 1')),
+            ElevatedButton(onPressed: null, child: Text('Answer 2')),
+            ElevatedButton(onPressed: null, child: Text('Answer 3')),
+          ],
+        ),
       ),
     );
   }
