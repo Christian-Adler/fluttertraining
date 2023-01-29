d67c9439b4eaf5fdabf16f319b08345716febaa8
Christian Adler <chris.adler@gmx.de>
2023-01-29 06:13:38 +0100

    reverse chart

--- 04 widgets_styling_logic/expenses/lib/widgets/chart_bar.dart
+++ 04 widgets_styling_logic/expenses/lib/widgets/chart_bar.dart
@@ -11,7 +11,10 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        FittedBox(child: Text('${spendingAmount.toStringAsFixed(0)}')),
+        Container(
+            height: 16,
+            child: FittedBox(
+                child: Text('${spendingAmount.toStringAsFixed(0)} €'))),
         SizedBox(
           height: 4,
         ),
