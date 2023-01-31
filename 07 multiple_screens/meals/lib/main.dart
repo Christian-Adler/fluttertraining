7cc2139285615fd29ad56b7d144f26b8a84981e9
Christian Adler <chris.adler@gmx.de>
2023-01-31 19:51:48 +0100

    add custom fonts and theme

--- 07 multiple_screens/meals/lib/main.dart
+++ 07 multiple_screens/meals/lib/main.dart
@@ -14,7 +14,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+            .copyWith(
+                secondary: Colors.amber,
+                background: const Color.fromRGBO(255, 254, 229, 1)),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              labelLarge: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+                fontFamily: 'RobotoCondensed',
+              ),
+              labelMedium: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+                fontFamily: 'RobotoCondensed',
+              ),
+              titleLarge: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Raleway',
+              ),
+            ),
       ),
       home: const CategoriesScreen(),
     );
