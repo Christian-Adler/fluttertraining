f96e00f49277e76f0c73a8870e71c0575b92902c
Christian Adler <chris.adler@gmx.de>
2023-02-01 20:19:36 +0100

    finish meal item

--- 07 multiple_screens/meals/lib/widgets/meal_item.dart
+++ 07 multiple_screens/meals/lib/widgets/meal_item.dart
@@ -11,6 +11,32 @@ class MealItem extends StatelessWidget {
 
   void _selectMeal() {}
 
+  String get complexityText {
+    switch (meal.complexity) {
+      case Complexity.Challenging:
+        return 'Challenging';
+      case Complexity.Simple:
+        return 'Simple';
+      case Complexity.Hard:
+        return 'Hard';
+      default:
+        return 'unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (meal.affordability) {
+      case Affordability.Affordable:
+        return '€';
+      case Affordability.Pricey:
+        return '€€';
+      case Affordability.Luxurious:
+        return '€€€';
+      default:
+        return 'unknown';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -36,17 +62,61 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
-                Text(
-                  meal.title,
-                  style: const TextStyle(
-                    fontSize: 26,
-                    color: Colors.white,
+                Positioned(
+                  bottom: 20,
+                  right: 20,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black45,
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      meal.title,
+                      style: const TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
                   ),
-                  softWrap: true,
-                  overflow: TextOverflow.fade,
                 )
               ],
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        const Icon(Icons.schedule),
+                        const SizedBox(
+                          width: 6,
+                        ),
+                        Text('${meal.duration} min'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Icon(Icons.work),
+                        const SizedBox(
+                          width: 6,
+                        ),
+                        Text(complexityText),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Icon(Icons.attach_money),
+                        const SizedBox(
+                          width: 6,
+                        ),
+                        Text(affordabilityText),
+                      ],
+                    ),
+                  ]),
+            ),
           ],
         ),
       ),
