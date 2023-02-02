875c435d586862db9dde506a9c1661582bd2dd29
Christian Adler <chris.adler@gmx.de>
2023-02-02 04:10:27 +0100

    add more info to meal details screen

--- 07 multiple_screens/meals/lib/screens/meal_detail_screen.dart
+++ 07 multiple_screens/meals/lib/screens/meal_detail_screen.dart
@@ -9,11 +9,53 @@ class MealDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context)?.settings.arguments as String;
-    final meal =
-        DUMMY_MEALS.where((element) => element.id == mealId).toList()[0];
+    final meal = DUMMY_MEALS.firstWhere((m) => m.id == mealId);
     return Scaffold(
       appBar: AppBar(title: Text(meal.title)),
-      body: Text('text'),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              meal.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Ingredients',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ),
+          Container(
+            height: 200,
+            width: 300,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10)),
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
+            child: ListView.builder(
+              itemBuilder: (ctx, idx) {
+                return Card(
+                  color: Theme.of(context).colorScheme.secondary,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                    child: Text(
+                      meal.ingredients[idx],
+                    ),
+                  ),
+                );
+              },
+              itemCount: meal.ingredients.length,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
