46a7be8a2c537d37faa751845331608004bc1011
Christian Adler <chris.adler@gmx.de>
2023-02-03 19:30:34 +0100

    08 state management app initial

---
+++ 08 state_management/shop/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: _MyHomePage(),
+    );
+  }
+}
+
+class _MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Shop'),
+      ),
+      body: const Center(
+        child: Text('Navigation Time!'),
+      ),
+    );
+  }
+}
