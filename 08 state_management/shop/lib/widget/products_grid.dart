c50e8f6baa405866f33b759e90d18fe48949df02
Christian Adler <chris.adler@gmx.de>
2023-02-04 06:46:57 +0100

    introduce provider for state managing

---
+++ 08 state_management/shop/lib/widget/products_grid.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/products_provider.dart';
+
+import './productItem.dart';
+
+class ProductsGrid extends StatelessWidget {
+  const ProductsGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final productsProviderData = Provider.of<ProductsProvider>(context);
+    final products = productsProviderData.items;
+
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        childAspectRatio: 3 / 2,
+        mainAxisSpacing: 10,
+      ),
+      padding: const EdgeInsets.all(10),
+      itemCount: products.length,
+      itemBuilder: (context, index) => ProductItem(
+        products[index],
+      ),
+    );
+  }
+}
