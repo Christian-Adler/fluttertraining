5ff0066a83033a147f807f9097963e0740853371
Christian Adler <chris.adler@gmx.de>
2023-02-04 16:11:54 +0100

    use of context not necessary here

--- 08 state_management/shop/lib/main.dart
+++ 08 state_management/shop/lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (context) => ProductsProvider(),
+      create: (_) => ProductsProvider(),
       child: MaterialApp(
         title: 'Shop',
         theme: ThemeData(
