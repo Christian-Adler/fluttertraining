f65c2ded7359bc3353393b6d2969b636ce5d2393
Christian Adler <chris.adler@gmx.de>
2023-02-11 21:06:52 +0100

    store future in stateful widget to ensure only one execution
    prevents multiple http requests if widget is rebuild

--- 10 http_requests/shop/lib/screen/orders_screen.dart
+++ 10 http_requests/shop/lib/screen/orders_screen.dart
@@ -5,10 +5,29 @@ import 'package:shop/widget/order_list_item.dart';
 import '../providers/orders.dart';
 import '../widget/app_drawer.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
 
   const OrdersScreen({Key? key}) : super(key: key);
+
+  @override
+  State<OrdersScreen> createState() => _OrdersScreenState();
+}
+
+class _OrdersScreenState extends State<OrdersScreen> {
+  late Future _ordersFuture;
+
+  Future _obtainOrdersFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  }
+
+  @override
+  void initState() {
+    // Falls build aufgrund anderer state-Aenderungen mehrmals ausgefuehrt wuerde.
+    // Future nur einmal ausfuehren und speichern.
+    _ordersFuture = _obtainOrdersFuture();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +37,7 @@ class OrdersScreen extends StatelessWidget {
       ),
       drawer: const AppDrawer(),
       body: FutureBuilder(
-        future: Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+        future: _ordersFuture,
         builder: (czx, dataSnapshot) {
           if (dataSnapshot.connectionState == ConnectionState.waiting) {
             return const Center(
