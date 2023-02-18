cf2a0f2cc6ee9a1b800a81c7da9f63b183eb5cd1
Christian Adler <chris.adler@gmx.de>
2023-02-18 05:36:55 +0100

    use FadeInImage to smoothly replace a placeholder with the loaded image

--- 12 animation/shop/lib/widget/product_item.dart
+++ 12 animation/shop/lib/widget/product_item.dart
@@ -63,8 +63,9 @@ class ProductItem extends StatelessWidget {
           onTap: () {
             Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: product.id);
           },
-          child: Image.network(
-            product.imageUrl,
+          child: FadeInImage(
+            placeholder: const AssetImage('assets/images/product-placeholder.png'),
+            image: NetworkImage(product.imageUrl),
             fit: BoxFit.cover,
           ),
         ),
