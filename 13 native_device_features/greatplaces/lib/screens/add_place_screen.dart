5922b39fc8fe0bf1bd8643b8d8097e6d5eb26218
Christian Adler <chris.adler@gmx.de>
2023-02-20 17:03:12 +0100

    prepare location input

--- 13 native_device_features/greatplaces/lib/screens/add_place_screen.dart
+++ 13 native_device_features/greatplaces/lib/screens/add_place_screen.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:greatplaces/provider/great_places.dart';
 import 'package:greatplaces/widgets/image_input.dart';
+import 'package:greatplaces/widgets/location_input.dart';
 import 'package:provider/provider.dart';
 
 class AddPlaceScreen extends StatefulWidget {
@@ -51,6 +52,8 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     ),
                     const SizedBox(height: 10),
                     ImageInput(_selectImage),
+                    const SizedBox(height: 10),
+                    const LocationInput(),
                   ],
                 ),
               ),
